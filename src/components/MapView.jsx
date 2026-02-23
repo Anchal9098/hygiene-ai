89207825562d9bd6ac808da10673708ddb6e9872
Anchal Gupta <anchal9098@gmail.com>
2026-02-23 11:34:57 +0530

    added leaflet map and nagpur setup

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useRef } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
 export default function MapView() {
-  return (
-    <div className="bg-gray-200 h-64 rounded-xl flex items-center justify-center">
-      Map Integration Here
-    </div>
-  )
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (mapRef.current) return; // prevents double init
+
+    const map = L.map("map").setView([21.1458, 79.0882], 13);
+    mapRef.current = map;
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "© OpenStreetMap",
+    }).addTo(map);
+
+   
+
+  L.marker([21.1458, 79.0882]).addTo(map).bindPopup("Nagpur Location");  }, []);
+
+  return <div id="map" className="h-64 w-full rounded-xl"></div>;
 }
